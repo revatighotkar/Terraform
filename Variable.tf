a75d83babb3acde398de2eef1693ab1c7ab24db9
revatighotkar <revatighotkar1999@gmail.com>
2025-08-07 02:06:33 +0530

    variables removed

--- Variable.tf
+++ Variable.tf
@@ -1,34 +1,34 @@
 variable "Aws_region" {
-  type    = string
-  default = "us-west-1"
+  type = string
+
 }
 variable "cidr_block1" {
-  type    = string
-  default = "us-west-1"
+  type = string
+
 
 }
 
 
 variable "cidr_block2" {
-  type    = string
-  default = "us-west-1a"
+  type = string
+
 
 
 }
 
 variable "cidr_block3" {
-  type    = string
-  default = "us-west-1b"
+  type = string
+
 }
 
 variable "availability_zone1" {
-  type    = string
-  default = "us-west-1"
+  type = string
+
 
 }
 
 variable "availability_zone2" {
-  type    = string
-  default = "us-west-1"
+  type = string
+
 
 }
