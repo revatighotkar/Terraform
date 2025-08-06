291249048edde83345c83d556da67ef2f4621dff
revatighotkar <revatighotkar1999@gmail.com>
2025-08-07 00:24:23 +0530

    secur way

---
+++ Variable.tf
@@ -0,0 +1,34 @@
+variable "Aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+variable "cidr_block1" {
+  type    = string
+  default = "us-west-1"
+
+}
+
+
+variable "cidr_block2" {
+  type    = string
+  default = "us-west-1a"
+
+
+}
+
+variable "cidr_block3" {
+  type    = string
+  default = "us-west-1b"
+}
+
+variable "availability_zone1" {
+  type    = string
+  default = "us-west-1"
+
+}
+
+variable "availability_zone2" {
+  type    = string
+  default = "us-west-1"
+
+}
--- main.tf
+++ main.tf
@@ -1,39 +1,37 @@
-provider "aws" {
-region = "us-west-1"
-access_key = "your_access_key"
-secret_key = "your_secret_key"                         
-                
+ provider "aws" {
+  region = var.Aws_region
+
 }
 
 # VPC
-resource "aws_vpc" " VPC_Cloud " {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true             
+resource "aws_vpc" "VPC_Cloud" {
+  cidr_block         = var.cidr_block1
+  enable_dns_support = true
 }
 # Public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.VPC_Cloud.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"  # Change based on your region
+  cidr_block              = var.cidr_block2
+  availability_zone       = var.availability_zone1
   map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet"
   }
-  }
+}
 #  Private subnet
-  resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.VPC.Cloud.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-1b"  # Change based on your region
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.VPC_Cloud.id
+  cidr_block              = var.cidr_block3        # "10.0.2.0/24"
+  availability_zone       = var.availability_zone2 # Change based on your region
   map_public_ip_on_launch = true
   tags = {
     Name = "private-subnet"
   }
-  }
+}
 #  Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.VPC.Cloud.id
+  vpc_id = aws_vpc.VPC_Cloud.id
 
   tags = {
     Name = "main-igw"
