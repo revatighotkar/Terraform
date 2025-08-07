bd60403eb7e7ac0d16087af0434f47c9a4ee9022
revatighotkar <revatighotkar1999@gmail.com>
2025-08-07 16:49:23 +0530

    done resolved

--- SecurityGrp.tf
+++ SecurityGrp.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "Route_Table" {
   vpc_id = aws_vpc.VPC_Cloud.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = ["0.0.0.0/0"]
     gateway_id = aws_internet_gateway.igw.id
   }
 }
